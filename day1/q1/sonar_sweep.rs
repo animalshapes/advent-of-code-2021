84911449fd76a4797ef11bead8a378b204002624
andrew <andrew.chen305@gmail.com>
2021-12-02 00:07:51 -0500

    working number 1

--- day1/q1/sonar_sweep.rs
+++ day1/q1/sonar_sweep.rs
@@ -1,11 +1,22 @@
-use std::env;
 use std::fs;
 
 fn main() {
     let filename = "input.txt";
 
-    let contents = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
 
-    println!("With text:\n{}", contents);
+    let numbers: Vec<i32> = contents
+        .split("\n")
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect();
+
+    let increasing: Vec<i32> = numbers
+        .iter()
+        .zip(numbers.iter().skip(1))
+        .map(|(a, b)| (b > a) as i32)
+        .collect();
+    
+    let sum: i32 = increasing.iter().sum();
+
+    println!("{}", sum);
 }
