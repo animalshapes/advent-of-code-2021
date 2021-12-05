1f187c818a3e0af6756702763c4c7cc66f4d9df0
andrew <andrew.chen305@gmail.com>
2021-12-05 13:29:46 -0500

    improvements

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,16 +1,16 @@
 use std::collections::HashSet;
 use std::{env, fs};
 
-fn check_board(board_combo: &[HashSet<u32>], draws: HashSet<u32>) -> bool {
+fn check_board(board_combo: &[HashSet<&u32>], draws: &HashSet<&u32>) -> bool {
     let num_matches = board_combo
         .iter()
-        .filter(|&combo| combo.is_subset(&draws))
+        .filter(|&combo| combo.is_subset(draws))
         .count();
     num_matches > 0
 }
 
-fn calculate_board_score(board_nums: &HashSet<u32>, draws: Vec<u32>) -> u32 {
-    let draws_set = draws.clone().into_iter().collect::<HashSet<_>>();
+fn calculate_board_score(board_nums: &HashSet<&u32>, draws: &[&u32]) -> u32 {
+    let draws_set = draws.iter().copied().collect::<HashSet<_>>();
 
     let init: u32 = 0;
     let remaining_sum = board_nums
@@ -50,46 +50,42 @@ fn main() {
 
     let board_nums = boards
         .iter()
-        .map(|board| board.clone().into_iter().flatten().collect::<HashSet<_>>())
+        .map(|board| board.iter().flatten().collect::<HashSet<_>>())
         .collect::<Vec<_>>();
 
     let board_combos = boards
         .iter()
         .map(|board| {
             let rows = board
-                .clone()
-                .into_iter()
-                .map(|row| row.into_iter().collect::<HashSet<_>>())
+                .iter()
+                .map(|row| row.iter().collect::<HashSet<_>>())
                 .collect::<Vec<_>>()
                 .into_iter();
             let cols = (0..board_size)
-                .map(|i| board.iter().map(|row| row[i]).collect::<HashSet<_>>())
+                .map(|i| board.iter().map(|row| &row[i]).collect::<HashSet<_>>())
                 .collect::<Vec<_>>()
                 .into_iter();
-            cols.chain(rows).collect::<Vec<_>>()
+            rows.chain(cols).collect::<Vec<_>>()
         })
         .collect::<Vec<_>>();
 
     let draws_sequence = (board_size..draws.len())
-        .map(|index| draws.clone().into_iter().take(index).collect::<Vec<_>>())
+        .map(|index| draws.iter().take(index).collect::<Vec<_>>())
         .collect::<Vec<_>>();
 
     for draw in draws_sequence {
-        let draw_set = draw.clone().into_iter().collect::<HashSet<_>>();
+        let draw_set = draw.iter().copied().collect::<HashSet<_>>();
 
         let bingos = board_combos
             .iter()
             .zip(board_nums.iter())
-            .filter(|(board_combo, _)| {
-                let draw_set_copy = draw_set.clone().into_iter().collect::<HashSet<_>>();
-                check_board(&board_combo[..], draw_set_copy)})
+            .filter(|(board_combo, _)| check_board(&board_combo[..], &draw_set))
             .map(|(_, nums)| nums)
             .collect::<Vec<_>>();
 
         if bingos.iter().len() > 0 {
-            let score = calculate_board_score(bingos[0], draw);
-            println!("{:?}", score);
-            println!("{:?}", bingos.iter().len());
+            let score = calculate_board_score(bingos[0], &draw);
+            println!("Part 1: {:?}", score);
             break;
         }
     }
