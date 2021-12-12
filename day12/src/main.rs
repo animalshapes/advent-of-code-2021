669c30398390d4f842ebe662cf27f2b789477844
andrew <andrew.chen305@gmail.com>
2021-12-12 15:37:13 -0500

    don't save full paths

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::collections::{HashMap, VecDeque};
 
 fn is_large_cave(cave: &str) -> bool {
     cave.chars().all(|c| c.is_ascii_uppercase())
@@ -11,25 +11,25 @@ fn convert_edges_to_map(edges: Vec<&str>) -> HashMap<&str, Vec<&str>> {
         let first = sides.next().expect("no first node");
         let second = sides.next().expect("no second node");
 
-        map.entry(first).or_insert(Vec::new()).push(second);
-        map.entry(second).or_insert(Vec::new()).push(first);
+        map.entry(first).or_insert_with(Vec::new).push(second);
+        map.entry(second).or_insert_with(Vec::new).push(first);
     }
     map
 }
 
-fn traverse_map_p1<'a, 'b>(map: &'b HashMap<&'a str, Vec<&'a str>>) -> Vec<Vec<&'a str>> {
+fn traverse_map_p1<'a, 'b>(map: &'b HashMap<&'a str, Vec<&'a str>>) -> i32 {
     let mut deq: VecDeque<(&str, Vec<&str>)> = VecDeque::from([("start", vec!["start"])]);
-    let mut paths: Vec<Vec<&str>> = Vec::new();
+    let mut paths: i32 = 0;
 
     while let Some((id, path)) = deq.pop_front() {
         let neighbors = map.get(id).expect("node does not exist in map");
         for &neighbor in neighbors {
             if is_large_cave(neighbor) || !path.contains(&neighbor) {
-                let mut new_path = path.clone();
-                new_path.push(neighbor);
                 if neighbor == "end" {
-                    paths.push(new_path);
+                    paths += 1
                 } else {
+                    let mut new_path = path.clone();
+                    new_path.push(neighbor);
                     deq.push_back((neighbor, new_path))
                 }
             }
@@ -39,41 +39,36 @@ fn traverse_map_p1<'a, 'b>(map: &'b HashMap<&'a str, Vec<&'a str>>) -> Vec<Vec<&
     paths
 }
 
-fn traverse_map_p2<'a, 'b>(map: &'b HashMap<&'a str, Vec<&'a str>>) -> Vec<Vec<&'a str>> {
-    let mut deq: VecDeque<(&str, Vec<&str>)> = VecDeque::from([("start", vec!["start"])]);
-    let mut paths: Vec<Vec<&str>> = Vec::new();
+fn traverse_map_p2<'a, 'b>(map: &'b HashMap<&'a str, Vec<&'a str>>) -> i32 {
+    let mut deq: VecDeque<(&str, Vec<&str>, bool)> =
+        VecDeque::from([("start", vec!["start"], false)]);
+    let mut paths: i32 = 0;
 
-    while let Some((id, path)) = deq.pop_front() {
+    while let Some((id, path, dupe)) = deq.pop_front() {
         let neighbors = map.get(id).expect("node does not exist in map");
         for &neighbor in neighbors {
-            let visited: HashSet<&str> = path
-                .iter()
-                .copied()
-                .filter(|&id| !is_large_cave(id))
-                .collect();
-            if visited.len()
-                < path
-                    .iter()
-                    .copied()
-                    .filter(|&id| !is_large_cave(id))
-                    .count()
-            {
+            if dupe {
                 if is_large_cave(neighbor) || !path.contains(&neighbor) {
-                    let mut new_path = path.clone();
-                    new_path.push(neighbor);
                     if neighbor == "end" {
-                        paths.push(new_path);
+                        paths += 1;
                     } else {
-                        deq.push_back((neighbor, new_path))
+                        let mut new_path = path.clone();
+                        new_path.push(neighbor);
+                        deq.push_back((neighbor, new_path, dupe))
                     }
                 }
             } else if neighbor != "start" {
-                let mut new_path = path.clone();
-                new_path.push(neighbor);
                 if neighbor == "end" {
-                    paths.push(new_path);
+                    paths += 1;
                 } else {
-                    deq.push_back((neighbor, new_path))
+                    let mut new_path = path.clone();
+                    new_path.push(neighbor);
+                    let dupe = path
+                        .iter()
+                        .copied()
+                        .filter(|&cave| !is_large_cave(cave))
+                        .any(|cave| cave == neighbor);
+                    deq.push_back((neighbor, new_path, dupe));
                 }
             }
         }
@@ -89,8 +84,8 @@ fn main() {
     let map = convert_edges_to_map(edges);
 
     let paths_p1 = traverse_map_p1(&map);
-    println!("Part 1: {:?}", paths_p1.len());
+    println!("Part 1: {:?}", paths_p1);
 
     let paths_p2 = traverse_map_p2(&map);
-    println!("Part 2: {:?}", paths_p2.len());
+    println!("Part 2: {:?}", paths_p2);
 }
